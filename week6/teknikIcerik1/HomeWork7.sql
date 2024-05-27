6276cf098398edcd971f7989c70ef4887c1403b2
yakuperenermurat <yakuperenermurat@gmail.com>
2024-05-27 19:22:25 +0300

    HomeWork7 update

--- week6/teknikIcerik1/HomeWork7.sql
+++ week6/teknikIcerik1/HomeWork7.sql
@@ -1,11 +1,12 @@
 --film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
-SELECT rating FROM film
+SELECT rating , COUNT(*) FROM film
 GROUP BY rating ;
 
 --film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
 SELECT replacement_cost ,COUNT(*)  FROM film
 GROUP BY replacement_cost 
-HAVING COUNT(*) > 50;
+HAVING COUNT(*) > 50
+ORDER BY replacement_cost;
 
 --customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir? 
 SELECT store_id,COUNT(*) FROM customer
